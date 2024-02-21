83c995461204c78d053bbe421f3396251e5e6d75
Nelly <nelly@Nellys.localdomain>
2024-02-21 16:42:14 -0400

    Functioning and loops

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -4,17 +4,13 @@
  *
  * Return: Always (Success)
  */
-
 void print_alphabet(void)
 {
 	char c;
 	for (c = 'a'; c <= 'z'; c++)
 	{
-		_putchar(c);
+	_putchar(c);
 	}
-
 	_putchar('\n');
-
 	return (0);
 }
-
