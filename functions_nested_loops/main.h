8eb7666fac8ac293fe5ef1ea991a48587d25928b
Nelly <nelly@Nellys.localdomain>
2024-02-21 14:44:59 -0400

    Functioning and loops

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,11 +1,1 @@
-#include "main.h"
-/**
- * main - Printing _putchars that returns an entire zero
- *
- * Return: Always (Success)
- */
-int main (void)
-{
-  puts("_putchar");
-  return 0;
-}
+int _putchar(char c)
