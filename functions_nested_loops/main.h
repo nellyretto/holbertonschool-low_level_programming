adf5b4306eeefed9b42e34a6af79dd08f3dde4f1
Nelly <nelly@Nellys.localdomain>
2024-02-21 11:44:22 -0400

    Functioning and loops

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -7,11 +7,6 @@
  */
 int main(void)
 {
-    print_putchar(m);
-    print_putchar(a);
-    print_putchar(i);
-    print_putchar(n);
-    print_putchar(.);
-    print_putchat(h);
+    print_putchar(main.h);
     return (0);
 }
