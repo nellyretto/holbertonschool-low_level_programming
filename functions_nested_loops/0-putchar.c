25b166a3aa267daef301030d7573052da3eba9db
Nelly <nelly@Nellys.localdomain>
2024-02-21 15:54:14 -0400

    Functioning and loops

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,21 +1,25 @@
 #include "main.h"
+
 /**
  * main - printing putchar
  *
- * Return: Always (Success)
+ * Return: 0 Always (Success)
  *
  */
+
 int main(void)
 {
+
 	_putchar('_');
 	_putchar('p');
-	_putchar(‘u’);
-	_putchar(‘t’);
-	_putchar(‘c’);
-	_putchar(‘h’);
-	_putchar(‘a’);
-	_putchar(‘r’);
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
+
 }
