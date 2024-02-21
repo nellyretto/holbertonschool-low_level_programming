cf42dc2eac822b78ff149a7de8dd82c6bd30bab0
Nelly <nelly@Nellys.local>
2024-02-21 19:04:43 -0400

    Functioning and loops

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
- * main - printing the alphabet 10 times
+ * print_alphabettentimes - printing the alphabet 10 times
  *
- * Return: 0 Always (Success)
  */
 
-int main(void)
+void print_alphabettentimes(void)
 {
 	char 10times;
 	for (10times = 'a'; 10times < 10 'z'; 10times)
 	{
-		_putchar(10times);
+	_putchar(10times);
 	}
-		_putchar('n\');
+	_putchar('n\');
 }
