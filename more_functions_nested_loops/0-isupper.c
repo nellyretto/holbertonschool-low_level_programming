0d91256d36c0b07fd214e637cf15aa627762758e
Nelly <nelly@Nellys.localdomain>
2024-02-22 10:38:47 -0400

    Loops loops loops

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * isupper - checking for upercase letters
+ *@c: uppercase character
+ * Return: 1 if c is uppercase 1 if otherwise
+ */
+int isupper(int c)
+{
+	return (c <= 'a' && c >= 'z');
+}
