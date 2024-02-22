167d81ae8e8b5f674fcf644854a4bbbec9305001
Nelly <nelly@Nellys.localdomain>
2024-02-22 16:41:14 -0400

    Loops loops loops

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * @a: to be multiplied
+ * @b: to be mutliplied
+ * _mult - multylipying two intengers
+ */
+
+int mul(int a, int b)
+{
+	int Nelly;
+	Nelly = (a * b);
+
+	return (Nelly);
+}
