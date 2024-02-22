66aa681642b437eafd2612081af7d2e935d4ad8f
Nelly <nelly@Nellys.local>
2024-02-21 22:06:43 -0400

    Functioning and loops

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - returning the result
+ * @a: adding this to b
+ * @b: adding this to a
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
