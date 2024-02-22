d2bdc2332484703b93d44c31737c6d3a0572d8a3
Nelly <nelly@Nellys.local>
2024-02-21 21:46:59 -0400

    Functioning and loops

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_last_digit - shows the last part of a number
+ *
+ * Return: digit las value
+ *
+ */
+int print_last_digit(int n)
+{
+	int a;
+
+	if (n < 0)
+		n = -n;
+
+	a = n % 10;
+
+	if (a < 0)
+		a = -a;
+
+	_putchar(a + '0');
+
+	return (a);
+}
