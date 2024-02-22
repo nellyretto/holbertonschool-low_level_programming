1b4ab0aee6ec9efca5d5830c1119ed6c3f873599
Nelly <nelly@Nellys.localdomain>
2024-02-22 10:49:14 -0400

    Loops loops loops

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -2,7 +2,7 @@
 /**
  * isupper - checking for upercase letters
  *@c: _uppercase character
- * Return: 1 if c is uppercase 1 if otherwise
+ * Return: 1 if c is uppercase, 0 if otherwise
  */
 int _isupper(int c)
 {
