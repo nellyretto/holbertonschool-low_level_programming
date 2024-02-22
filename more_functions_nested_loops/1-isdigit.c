0830b99f62993d9ef61652454d9beaa261d83e1c
Nelly <nelly@Nellys.localdomain>
2024-02-22 14:32:10 -0400

    Loop loops loops

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * _isdigit - checking digits  * @c: checking this variable
+ * _isdigit - checking digits from 0 to 9
+ * @c: checking this variable
  * Return: return 1 if c is a digit
  */
 
