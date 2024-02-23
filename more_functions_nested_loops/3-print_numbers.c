8331fa895bfd1d387b2950cc9c805f454fa03dcb
Nelly <danielys2416@gmail.com>
2024-02-22 23:18:59 -0400

    Update 3-print_numbers.c

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_numbers - prints the numbers, from 0 to 9,
- * followed by a new line
+ * print_numbers - printing from 0 to 9 with a new line
+ * 
  */
 void print_numbers(void)
 {
