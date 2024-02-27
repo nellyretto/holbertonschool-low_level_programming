c5bb68d231c1e044f5738e894ad7e891d68aea16
Nelly <nelly@Nellys.local>
2024-02-26 20:23:35 -0400

    Pointers

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * swap_int - swaping two intengers
+ *
+ * @a: variable to swap
+ * @b: variable to swap
+ *
+ */
+
+void swap_int(int *a, int *b)
+
+{
+
+	int a;
+	int b;
+
+}
+
+
