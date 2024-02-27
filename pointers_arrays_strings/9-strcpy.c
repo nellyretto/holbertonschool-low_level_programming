97cfa1de5cba39f8886f610dab611f47a8691055
Nelly <nelly@Nellys.local>
2024-02-26 23:06:41 -0400

    copying strings

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -2,8 +2,6 @@
 
 /**
  * *_strcpy - copies the string pointed to by src
- * including the terminating null byte (\0)
- * to the buffer pointed to by dest
  * @dest: pointer to the buffer in which we copy the string
  * @src: string to be copied
  *
