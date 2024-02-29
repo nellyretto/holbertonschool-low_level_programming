90f54f08e35a773f07c7856fa447e53aedcf4c84
Nelly <nelly@Nellys.local>
2024-02-28 22:15:04 -0400

    Comapring two strings

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcmp - comparing two strings
+ * @s1: first comparing strin
+ * @s2: first comparing string 
+ *
+ * Return: less than 0 if s1 is less than s2, 0 if they're equal,
+ * more than 0 if s1 is greater than s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
