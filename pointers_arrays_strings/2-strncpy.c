56ec7cc45169e2645c3ee8bb4d2473c420c6d689
Nelly <nelly@Nellys.local>
2024-02-28 22:13:12 -0400

    Copying a string

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncpy - copyingg a string
+ * @dest: string
+ * @src: secind string
+ * @n: bytes
+ *
+ * Return: pointer to the resulting string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
