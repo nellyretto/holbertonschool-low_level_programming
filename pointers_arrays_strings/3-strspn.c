56189688b6e6e433448a5fac6bf4b908262dc2d9
Nelly <nelly@Nellys.local>
2024-02-29 22:42:55 -0400

    getting the lenght of a prefix substring

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+# include 
+
+/**
+ * _strspn - getting the lenght of a prefix substring
+ * @s: returning the bytes here
+ *
+ *
+ */
+
+_strspn
+
+{
+	int i, j;
+	int c = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] != 32)
+		{
+			for (j = 0; accept[j] != '\0'; j++)
+			{
+				if (s[i] == accept[j])
+					c++;
+			}
+		}
+		else
+			return (c);
+
+	}
+	return (c);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -18,5 +18,4 @@ char *_strncat(char *dest, char *src, int n);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
-
-#endif
+unsigned int _strspn(char *s, char *accept);#endif
