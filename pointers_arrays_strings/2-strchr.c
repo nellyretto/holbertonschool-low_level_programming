6fb78c6a3999183561db7b8c0dcdfbba1ebb37d1
Nelly <nelly@Nellys.local>
2024-02-29 22:35:01 -0400

    Locating character in string

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+# include "main.h"
+
+/**
+ * _strchr - locating character in a string
+ * Return: returning pointer in the s string 
+ *
+ */
+
+char *_strchr(char *s, char c);
+
+{
+	while (*s)
+	{
+		if (*s != c)
+			s++;
+		else
+			return (s);
+	}
+	if (c == '\0')
+		return (s);
+
+	return (NULL);
+}
+
+
