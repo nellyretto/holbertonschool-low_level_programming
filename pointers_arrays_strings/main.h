ac4cba0adca329823b9985d935fc2cbfb1dbd80d
Nelly <nelly@Nellys.local>
2024-02-29 22:20:40 -0400

    Filling memory with constant bytes

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -15,5 +15,6 @@ char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+char *_memset(char *s, char b, unsigned int n);
 
 #endif
