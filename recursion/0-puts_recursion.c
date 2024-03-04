d816f8edcf0f4a05b16433ac1add3954745601c0
Nelly <nelly@Nellys.local>
2024-03-04 18:29:20 -0400

    Printing a string with a new line

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *
+ * _puts_recursion - printing a string with a new line
+ *
+ * 
+ */
+
+{
+
+	char s [] = "This string ends by a new line. \n";
+}
+
+
--- recursion/main.h
+++ recursion/main.h
@@ -1,1 +1,9 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
+
 void _puts_recursion(char *s);
+
+
+
+#endif
