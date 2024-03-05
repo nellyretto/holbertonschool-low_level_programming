0d3a74fa01110c33c7fee0a2cab552339fd43f6e
Nelly <nelly@Nellys.local>
2024-03-05 19:34:16 -0400

    Printing own name followed by a new line

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -9,13 +9,8 @@
  *
  */
 
-int main(int argc, char *argv[])
 {
-    int i;
-    printf("%d\n",argc);
-    for(i=0;i<argc-1;i++)
-    {
-        printf("%s",*argv[i]);
-    }
-    return 0;
+	printf("%s\n", *argv);
+
+	return (0);
 }
