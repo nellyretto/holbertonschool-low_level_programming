5bb96e196269a31b04292633bda9d0f81b9faf21
Nelly <nelly@Nellys.local>
2024-03-05 19:28:09 -0400

    Printing own name followed by a new line

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,4 +1,13 @@
 #include<stdio.h>
+
+/**
+ * whatsmyname - printing own name with new line
+ * @argc: argument count
+ * @agrv: argument vector
+ * Return: Always 0 (Success)
+ *
+ */
+
 int main(int argc, char *argv[])
 {
     int i;
