af05175cff680b6e99627fc64809e1abeeb84587
Nelly <nelly@Nellys.local>
2024-03-06 22:46:30 -0400

    Returnin pointer to 2 dimensional array of intengers

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - returning a pointer to 2 dimensional array of intengers
+ * @width: width of int size
+ * height: height of int size
+ *
+ * Return: returning NULL on failure
+ *
+ */
+
+int **alloc_grid(int width, int height)
+
+
