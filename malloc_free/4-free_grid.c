e4d7588b2b796a2868152e35c0f82ec88d1630d7
Nelly <nelly@Nellys.local>
2024-03-06 22:55:48 -0400

    Freeing a 2 dimensional grid

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - freeing a 2dimensional grid
+ * @height: grid height
+ * @grid: 2 dimensional grid
+ *
+ */
+
+void free_grid(int **grid, int height)
+
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
