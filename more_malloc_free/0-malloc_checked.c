dd2f1e359a65cc0fc2cb3d0e8a43083284290da7
Nelly <nelly@Nellys.local>
2024-03-07 20:32:02 -0400

    Allocating memory in space

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocating memory
+ * Return: pointer to allocated memory
+ * @b: int variable
+ *
+ */
+
+void *malloc_checked(unsigned int b)
+
+{
+	ptr = (int *)malloc(10 * sizeof(int b));
+
+	_putchar("Allocating new memory.\n");
+}
+
+
