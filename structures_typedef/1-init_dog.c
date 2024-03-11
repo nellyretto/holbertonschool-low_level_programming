b2ea6d23d2060f8ddf06a8a68040faa55357b8dc
Nelly <nelly@Nellys.local>
2024-03-11 19:22:31 -0400

    Initializating  variable of struct dog

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - initializating variable of struct dog
+ *
+ *
+ *
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+
+struct Dog
+{
+int age;
+int height;
+}
