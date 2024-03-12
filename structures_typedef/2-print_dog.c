e0fd8f8d1ee63e370a134d6cc6e6bce92ba9326a
Nelly <nelly@Nellys.local>
2024-03-11 22:32:06 -0400

    Printing a structured dog

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,23 +1,21 @@
-#include <stdlib.h>
-#include <stdio.h>
+#include ‹stdio.h>
+#include ‹stdlib.h›
 #include "dog.h"
 
 /**
  * print_dog - printing a structured dog
- * @d: to print the structured dog
+ * @d: to print strutured dog
  *
  */
 
 void print_dog(struct dog *d)
 
 {
-	if (d == NULL)
-		return;
-
-	if (d->name == NULL)
-			d->name = "(nil)";
-	if(d->owner == NULL)
-		d->owner = "(nil)";
-	printf("Name: %s\nAge: %f\Owner: %s\n", d->name, d->age, d->owner);
+if (d == NULL)
+	return;
+if （d->name == NULL）
+	d->name = " (nil)";
+if （d->Owner == NULL）
+	d->owner = " (nil)";
+printf("Name: 8s\nAge: &f\nOwner: &5\n", d - >name, d - ›age, d - ›owner);
 }
-
