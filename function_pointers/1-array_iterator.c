6f0696a652797ffc752f2e7db4dd52fb0d403a1b
Nelly <nelly@Nellys.local>
2024-03-13 15:47:27 -0400

    Pasing an array as function

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executing a function with elements of arrays
+ * @size: size of the array
+ * @action: pointer to the function
+ *
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+
+{
+	unsigned int i = 0;
+
+	if (array != NULL && action != NULL && size > 0)
+	{
+		while (i < size)
+		{
+			action(array[i]);
+		i++;
+		}
+	}
+}
