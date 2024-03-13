31d341b796785da19464076f0a934be83554273e
Nelly <nelly@Nellys.local>
2024-03-13 18:03:36 -0400

    Searching for an intenger

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -7,23 +7,27 @@
  * @array: array to be worked with
  * @cmp: pointer to the function
  * @size: number of elements in the array
- *
+ * Return: int_index returns the index of
+ * the first element for which the cmp function does not return 0
+ * If no element matches, return -1
+ * If size <= 0, return -1
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
 
 {
-	int n = 0;
-	if (size > 0)
-	{
-		if (array != NULL && cmp != NULL)
+	int i = 0;
+		if (array == NULL || cmp == NULL)
 		{
-			while (n < size)
-			if (cmp)array[n]))
-				return (n);
-			n++;
+			return (-1);
 		}
-	}
+		while (i < size)
+			{
+			if (cmp(array[i]))
+				{
+				return (i);
+				i++;
+				}
+			}
+	return (-1);
 }
-
-
