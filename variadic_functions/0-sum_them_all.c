a1a0bcd69f7a4fb06dd4904389943e9c497ff01b
Nelly <nelly@Nellys.local>
2024-03-14 20:19:30 -0400

    Returnin all parameters

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - returning all parameters
+ * @n: parameters
+ * Return; if n == 0, return 0
+ *
+ */
+
+int sum_them_all(const unsigned int n, ...);
+
+{
+	va_list ap;
+	unsigned int i, sum = 0;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
+
+	va_end(ap);
+
+	return (sum);
+}
