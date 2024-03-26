2cc9b6c5fa3b504312b9f5512a90df4d16e7968f
Nelly <nelly@Nellys.local>
2024-03-26 00:13:12 -0400

    Adding and trying third task

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,37 +1,46 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "lists.h"
 
 /**
- * *add_node - adding a new node at the begginign of a linked list
- * @head: functions as a double pointer
+ * add_node - adding a new node at the beggining of a linked list
+ * @head: functions as a double
  * @str: string to be manipulated
  *
- * Return: address of the new element, if NULL then fails
- *
+ * Return: address of new element, NULL if it fails
  */
 
 list_t *add_node(list_t **head, const char *str)
 
 {
-	list_t *new_node = malloc(sizeof(list_t));
-	if (new_node == NULL) {
-		return NULL;
-	}
+    list_t *new_node;
+    char *dup_str;
+    unsigned int len = 0;
 
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return NULL;
-	}
 
-	 new_node->len = strlen(str);
+    dup_str = strdup(str);
+    if (!dup_str)
+        return (NULL);
 
-	new_node->next = *head;
-		*head = new_node;
 
-	return new_node;
+    while (dup_str[len] != '\0')
+        len++;
+
+
+    new_node = (list_t *)malloc(sizeof(list_t));
+    if (!new_node)
+    {
+        free(dup_str);
+        return (NULL);
+    }
+
+
+    new_node->str = dup_str;
+    new_node->len = len;
+    new_node->next = *head;
+
+    *head = new_node;
+
+    return new_node;
 }
 
