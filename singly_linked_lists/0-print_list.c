e2ffcaa15c068962ec0de5ed4f32ac79c88848e1
Nelly <nelly@Nellys.local>
2024-03-25 22:34:41 -0400

    Adding firt archive of printing all elements on a list

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "list.h"
+
+/**
+ * print_list - printing all elements of a list, with singly linked lists
+ * @h: this is referring to head of the node
+ *
+ * Return: the number of nodes
+ */
+
+
+size_t print_list(const list_t *h)
+
+{
+    size_t nodes = 0;
+
+    while (h) {
+        if (h->str == NULL) {
+            printf("[0] (nil)\n");
+        } else {
+            printf("[%u] %s\n", h->len, h->str);
+        }
+        h = h->next;
+        nodes++;
+    }
+
+    return nodes;
+}
+
