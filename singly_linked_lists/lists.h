45b040b64577be79d7b379e4e8eb9433e771249d
Nelly <nelly@Nellys.local>
2024-03-25 22:53:40 -0400

    Adding the header file with its data structure

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,7 +1,15 @@
-#ifndef
-#define
+#ifndef LISTS_H
+#define LISTS_H
 
-int putchar (int character)
+typedef struct list_s
+{
+char *str;
+unsigned int len;
+struct list_s *next;
+} list_t;
+
+
+int putchar (int character);
 size_t print_list(const list_t *h);
 
 #endif
