217f40207bfa460d112d65385397c5557058099a
Nelly <nelly@Nellys.local>
2024-03-27 10:13:10 -0400

    Freeing a linked list

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_dlistint - Freeing a linked list
+ * Return: returning new nodes
+ *
+ */
+
+void free_dlistint(dlistint_t *head)
+
+{
+	dlistint_t *current = head;
+	dlistint_t *next_node = NULL;
+
+	while (current != NULL)
+    {
+		next_node = current->next;
+		free(current);
+		current = next_node;
+    }
+}
