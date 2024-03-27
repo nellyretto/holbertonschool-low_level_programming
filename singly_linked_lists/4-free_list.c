3eec706e7d74cd7d7e94cfb779979f56edfbbf3f
Nelly <nelly@Nellys.local>
2024-03-26 21:27:25 -0400

    Fixing compiling from last task

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 #include "lists.h"
 
-/*
+/**
  * free_list - freeing a linked list
  * @head: funcitons as a pointer
  * @str: string to be manipulated
  *
  * Return: returning new node
- *
+ */
 
 void free_list(list_t *head)
 
