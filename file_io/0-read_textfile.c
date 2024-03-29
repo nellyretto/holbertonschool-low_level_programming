e802c7a754468a24d223ce82b3cf9c24757900d5
Nelly <nelly@Nellys.local>
2024-03-28 20:34:47 -0400

    Fixinf betty comments

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -16,41 +16,46 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 
 {
-    int fd;
-    ssize_t nread, nwritten;
-    char *buffer;
+	int fd;
+	ssize_t nread, nwritten;
+	char *buffer;
 
-    if (filename == NULL) {
-        return 0;
-    }
+	if (filename == NULL)
+	{
+		return (0);
+	}
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1) {
-        return 0;
-    }
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
 
-    buffer = malloc(letters * sizeof(char));
-    if (buffer == NULL) {
-        close(fd);
-        return 0;
-    }
+	buffer = malloc(letters * sizeof(char));
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
 
-    nread = read(fd, buffer, letters);
-    if (nread == -1) {
-        free(buffer);
-        close(fd);
-        return 0;
-    }
+	nread = read(fd, buffer, letters);
+	if (nread == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
-    nwritten = write(STDOUT_FILENO, buffer, nread);
-    if (nwritten == -1 || nwritten < nread) {
-        free(buffer);
-        close(fd);
-        return 0;
-    }
+	nwritten = write(STDOUT_FILENO, buffer, nread);
+	if (nwritten == -1 || nwritten < nread)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
-    free(buffer);
-    close(fd);
+	free(buffer);
+	close(fd);
 
-    return nwritten;
+	return (nwritten);
 }
--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -2,9 +2,9 @@
 
 /**
  * create_file - crating a function that creates a file
+ * @filename: name of the crreated file
  *
- *
- *
+ * @text_content: NULL terminating string
  * Return: 1 on success -1 if it fails
  *
  */
@@ -12,28 +12,32 @@
 int create_file(const char *filename, char *text_content)
 
 {
-    int fd;
-    ssize_t nwritten;
+	int fd;
+	ssize_t nwritten;
 
-    if (filename == NULL) {
-        return -1;
-    }
+	if (filename == NULL)
+	{
+		return (-1);
+	}
 
-    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-    if (fd == -1) {
-        return -1;
-    }
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
 
-    if (text_content != NULL) {
-        nwritten = write(fd, text_content, strlen(text_content));
-        if (nwritten == -1) {
-            close(fd);
-            return -1;
-        }
-    }
+	if (text_content != NULL
+			{
+		nwritten = write(fd, text_content, strlen(text_content));
+		if (nwritten == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
 
-    close(fd);
-    return 1;
+	close(fd);
+	return (1);
 }
 
 
