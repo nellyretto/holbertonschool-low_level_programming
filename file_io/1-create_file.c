dcc50ab6e4a4d2596a8a159021bc2008e756ada1
Nelly <nelly@Nellys.local>
2024-03-28 20:37:50 -0400

    Fixing comments and compiling

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -26,8 +26,8 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	if (text_content != NULL
-			{
+	if (text_content != NULL)
+	{
 		nwritten = write(fd, text_content, strlen(text_content));
 		if (nwritten == -1)
 		{
